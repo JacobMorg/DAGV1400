a50cf71142e264d332315ffd3eae3111715800ee
Jacob <10878304@uvu.edu>
2025-03-02 19:34:20 -0700

    Challenge 7
    Couldn't get stamina regen to put a cap on the public float data or regen only when the character is grounded. Will have to explore more.

--- Game 1/Adventure Game/Assets/Adventure Game/SimpleTriggerEventBehaviour.cs
+++ Game 1/Adventure Game/Assets/Adventure Game/SimpleTriggerEventBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,21 @@ using UnityEngine.Events;
 public class SimpleTriggerEventBehaviour : MonoBehaviour
 {
     public UnityEvent triggerEvent;
-
+    public UnityEvent staminaEvent;
+    
     private void OnTriggerEnter(Collider other)
     {
         // Trigger the event and test with a debug message
         triggerEvent.Invoke();
         Debug.Log("Player interacted with the object!");
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            staminaEvent.Invoke();
+            Debug.Log("Player tried to jump!");
+        }
     }
 }
