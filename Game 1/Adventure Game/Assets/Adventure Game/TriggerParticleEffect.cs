b88c54df0c5b0a1ccc6a6db57e6b870cd75d165a
Jacob <10878304@uvu.edu>
2025-03-29 14:36:18 -0600

    Added Scripting 10 Challenge

--- Game 1/Adventure Game/Assets/Adventure Game/TriggerParticleEffect.cs
+++ Game 1/Adventure Game/Assets/Adventure Game/TriggerParticleEffect.cs
@@ -6,7 +6,11 @@ using UnityEngine;
 public class TriggerParticleEffect : MonoBehaviour
 {
     private ParticleSystem particleSystem; //Reference to the particle system
-    public int particleAmount = 10; // public variable that changes particle amount
+   
+    public int firstEmissionAmount = 10; // public variable for first emission
+    public int secondEmissionAmount = 20; // public variable for second emission
+    public int thirdEmissionAmount = 30; // public variable for third emission
+    public float delayBetweenEmissions = 0.5f; // time between emissions
 
     private void Start()
     {
@@ -18,7 +22,23 @@ public class TriggerParticleEffect : MonoBehaviour
         if (other.gameObject.GetComponent<CharacterController>())
             //sees if the object with the character controller triggers the event
         {
-            particleSystem.Emit(particleAmount); //Emits the amount stated previously
+            StartCoroutine(EmitParticlesCoroutine());
         }
+    }
+    
+    // Coroutine to emit particles with delay
+    private IEnumerator EmitParticlesCoroutine()
+    {
+        //first
+        particleSystem.Emit(firstEmissionAmount);
+        yield return new WaitForSeconds(delayBetweenEmissions);
+        
+        //second
+        particleSystem.Emit(secondEmissionAmount);
+        yield return new WaitForSeconds(delayBetweenEmissions);
+        
+        //third
+        particleSystem.Emit(thirdEmissionAmount);
+        yield return new WaitForSeconds(delayBetweenEmissions);
     }
 }
